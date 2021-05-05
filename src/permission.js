bd08a1c166b4e97d940abbac0393258d9a700278
Hhhr <Hhhr1214@gmail.com>
2021-05-05 16:43:28 +0800

    获取用户资料的时机

--- src/permission.js
+++ src/permission.js
@@ -1,12 +1,16 @@
 import router from '@/router'
 import store from '@/store'
 
-router.beforeEach((to, from, next) => {
+router.beforeEach(async(to, from, next) => {
   const whiteList = ['/login', '/404']
   if (store.getters.token) {
     if (to.path === '/login') {
       next('/')
     } else {
+      // 这里有了 token 又不是到登录页
+      // 后台正常放行的逻辑中
+      // 就需要获取用户资料
+      await store.dispatch('user/getUserInfo')
       next()
     }
   } else {
