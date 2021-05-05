7df57f2611294b93835dd78aed94adabf78dd5a2
Hhhr <Hhhr1214@gmail.com>
2021-05-05 16:42:21 +0800

    请求拦截器进行 token 的注入

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { Message } from 'element-ui'
-
+import store from '@/store'
 // create an axios instance
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
@@ -9,7 +9,20 @@ const service = axios.create({
 })
 
 // request interceptor
-service.interceptors.request.use()
+service.interceptors.request.use(
+  config => {
+    const token = store.getters.token
+    // 如果已经有 token 就带在 config 上面
+    if (token) {
+      config.headers.Authorization = 'Bearer ' + token
+    }
+    return config
+  },
+  err => {
+    Message.error(err.message)
+    return Promise.reject(err)
+  }
+)
 
 // 响应拦截器
 service.interceptors.response.use(
