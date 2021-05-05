3dc548075621878ffb8118df194ffa69bfb0c2ab
Hhhr <Hhhr1214@gmail.com>
2021-05-05 16:43:55 +0800

    每次进入只拿一遍数据, 之后的跳转就不再发请求

--- src/permission.js
+++ src/permission.js
@@ -10,7 +10,18 @@ router.beforeEach(async(to, from, next) => {
       // 这里有了 token 又不是到登录页
       // 后台正常放行的逻辑中
       // 就需要获取用户资料
-      await store.dispatch('user/getUserInfo')
+
+      // await store.dispatch('user/getUserInfo')
+
+      // 其实每次跳转页面都会经过这里
+      // 最好是先确认之前没有拿过数据, 再发请求
+      // 仍哦之前拿过, 直接放行即可
+      // if (!store.state.user.userInfo) {
+      // 直接判断一个空对象, 其实永远是 true
+      // 只能判断里面的其中一个属性在不在
+      if (!store.state.user.userInfo.userId) {
+        await store.dispatch('user/getUserInfo')
+      }
       next()
     }
   } else {
