e811f0e85e80cd69552133fe02940ff8592216f0
Hhhr <Hhhr1214@gmail.com>
2021-05-05 09:41:59 +0800

    新建了一个测试页, 配置了路由, 添加了一个登录表单

---
+++ src/views/test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <!-- el-form 表单的容器 -->
+    <el-form>
+      <!-- el-form-item 单个表单项的容器 -->
+      <el-form-item>
+        <!-- el-input 是输入框 -->
+        <el-input v-model="formData.mobile" />
+      </el-form-item>
+      <el-form-item>
+        <!-- el-input 是输入框 -->
+        <el-input v-model="formData.password" />
+      </el-form-item>
+      <el-form-item>
+        <el-button>登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {
+        mobile: '',
+        password: ''
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
