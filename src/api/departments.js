cf9f534f66cdf9470a57a457b77d0df15976f413
Hhhr <Hhhr1214@gmail.com>
2021-05-07 11:09:35 +0800

    部门的删除完毕

--- src/api/departments.js
+++ src/api/departments.js
@@ -1,7 +1,16 @@
 import request from '@/utils/request'
 
+// 获取列表
 export function getDepartments() {
   return request({
     url: '/company/department'
+  })
+}
+
+// 删除部门
+export function delDepartment(id) {
+  return request({
+    url: '/company/department/' + id,
+    method: 'delete'
   })
 }
--- src/views/departments/components/tree-tools.vue
+++ src/views/departments/components/tree-tools.vue
@@ -1,7 +1,7 @@
 <template>
   <el-row type="flex" justify="space-between" align="middle" style="width: 100%; height: 40px">
     <el-col>
-      <strong v-if="isRoot"><i class="el-icon-s-home" /> {{ treeNode.name }}</strong>
+      <strong v-if="isRoot"><i class="el-icon-s-home"/> {{ treeNode.name }}</strong>
       <span v-else>{{ treeNode.name }}</span>
     </el-col>
     <el-col :span="4">
@@ -12,13 +12,14 @@
           <!-- 下拉菜单 element -->
           <el-dropdown>
             <span>
-              操作<i class="el-icon-arrow-down" />
+              操作<i class="el-icon-arrow-down"/>
             </span>
             <!-- 下拉菜单 -->
             <el-dropdown-menu slot="dropdown">
               <el-dropdown-item>添加子部门</el-dropdown-item>
               <el-dropdown-item v-if="!isRoot">修改部门</el-dropdown-item>
-              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>
+              <!--              <el-dropdown-item v-if="!isRoot">删除部门</el-dropdown-item>-->
+              <el-dropdown-item v-if="!isRoot" @click.native="delDepartment">删除部门</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
         </el-col>
@@ -28,6 +29,8 @@
 </template>
 
 <script>
+import { delDepartment } from '@/api/departments'
+
 export default {
   props: {
     treeNode: {
@@ -37,6 +40,18 @@ export default {
     isRoot: {
       type: Boolean,
       required: true
+    }
+  },
+  methods: {
+    async delDepartment() {
+      // 二次校验
+      await this.$confirm('是否确认删除')
+      // 核心就是发请求
+      await delDepartment(this.treeNode.id)
+      // 提示用户
+      this.$message.success('删除成功')
+      // 更新页面
+      this.$emit('delDepartment')
     }
   }
 }
