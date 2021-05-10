3f31373212259cb57babd3a285019a2e10e88595
Hhhr <Hhhr1214@gmail.com>
2021-05-10 15:02:09 +0800

    封装上传组件

--- src/components/index.js
+++ src/components/index.js
@@ -2,8 +2,10 @@
 // 希望在这里祖册的所有组件
 // 在 main.js 当中只需要 use 即可使用
 import PageTools from '@/components/PageTools'
+import UploadExcel from '@/components/UploadExcel'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools)
+    Vue.component('UploadExcel', UploadExcel)
   }
 }
