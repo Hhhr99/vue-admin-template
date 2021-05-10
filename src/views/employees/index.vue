897cec0f92caf1451b2708187fc0efe5c09ff165
Hhhr <Hhhr1214@gmail.com>
2021-05-10 10:18:47 +0800

    数据渲染和分页功能

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -15,14 +15,22 @@
       </page-tools>
       <!-- 放置表格和分页 -->
       <el-card>
-        <el-table border>
-          <el-table-column label="序号" sortable=""/>
-          <el-table-column label="姓名" sortable=""/>
-          <el-table-column label="工号" sortable=""/>
-          <el-table-column label="聘用形式" sortable=""/>
-          <el-table-column label="部门" sortable=""/>
-          <el-table-column label="入职时间" sortable=""/>
-          <el-table-column label="账户状态" sortable=""/>
+        <el-table border :data="list">
+          <el-table-column label="序号" sortable="">
+            <template slot-scope="scope">
+              <!-- 如果想要得出真正当前页员工的在全部数据的索引
+              应该先算出上一页的最后一行
+              再次基础上, 当前页再 +1 +2 以此类推 -->
+              <!-- 或者可以这么想, 前面所有的总条数基础上 +1 +2 .... -->
+              {{ ((page.page - 1) * page.size) + (scope.$index + 1) }}
+            </template>
+          </el-table-column>
+          <el-table-column label="姓名" sortable="" prop="username"/>
+          <el-table-column label="工号" sortable="" prop="workNumber"/>
+          <el-table-column label="聘用形式" sortable="" prop="formOfEmployment"/>
+          <el-table-column label="部门" sortable="" prop="departmentName"/>
+          <el-table-column label="入职时间" sortable="" prop="timeOfEntry"/>
+          <el-table-column label="账户状态" sortable="" prop="enableState"/>
           <el-table-column label="操作" sortable="" fixed="right" width="280">
             <template>
               <el-button type="text" size="small">查看</el-button>
@@ -36,7 +44,9 @@
         </el-table>
         <!-- 分页组件 -->
         <el-row type="flex" justify="center" align="middle" style="height: 60px">
-          <el-pagination layout="prev, pager, next"/>
+          <el-pagination layout="prev, pager, next" :total="page.total" :page-size="page.size"
+                         @current-change="currentChange"
+          />
         </el-row>
       </el-card>
     </div>
@@ -65,6 +75,12 @@ export default {
       const { rows, total } = await getEmployeeList(this.page)
       this.page.total = total
       this.list = rows
+    },
+    currentChange(newPage) {
+      // 1. 修改翻页配置
+      this.page.page = newPage
+      // 2. 发个请求
+      this.getEmployeeList()
     }
   }
 }
