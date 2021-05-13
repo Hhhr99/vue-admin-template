3ca3bd57fc6fc35f56cce7e8c049b97986c110e5
Hhhr <Hhhr1214@gmail.com>
2021-05-13 16:09:53 +0800

    上传组件的创建和试用

---
+++ src/components/ImageUpload/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <!--
+      list-type 决定文件列表的显示方式
+     -->
+    <el-upload
+      list-type="picture-card"
+    >
+      <i class="el-icon-plus" />
+    </el-upload>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,11 @@
 // 在 main.js 当中只需要 use 即可使用
 import PageTools from '@/components/PageTools'
 import UploadExcel from '@/components/UploadExcel'
+import ImageUpload from '@/components/ImageUpload'
 export default {
   install(Vue) {
     Vue.component('PageTools', PageTools)
     Vue.component('UploadExcel', UploadExcel)
+    Vue.component('ImageUpload', ImageUpload)
   }
 }
