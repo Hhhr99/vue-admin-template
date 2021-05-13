e8fdb015e839b85f0d5f37d5a5800d63d6a7be33
Hhhr <Hhhr1214@gmail.com>
2021-05-13 17:43:31 +0800

    上传部分本地交互小结

--- src/components/ImageUpload/index.vue
+++ src/components/ImageUpload/index.vue
@@ -1,12 +1,19 @@
 <template>
   <div>
     <!--
+      必填
       action 必填的图片上传地址, 后面要对接腾讯云, 只是放一个 # 敷衍一下
+      file-list 是管理上传文件的数组
+
+      影响显示
       list-type 决定文件列表的显示方式
       :class 动态类名可以控制有图片时隐藏上传图标
 
-      file-list 是管理上传文件的数组
-      on-preview 指定点击放大镜后的处理，有了这个函数放大镜才会出现
+      各个阶段的钩子
+      on-preview 指定点击放大镜后的处理, 有了这个函数放大镜才会出现
+      on-remove 删除后触发
+      on-change 图片数据改变(添加)后触发
+      http-request 替代了默认 action 上传行为,为后续腾讯云连接座准备
      -->
     <el-upload
       action="#"
