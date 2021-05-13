6dbb1625f7396a6834c3952c0dca600ce0e32e26
Hhhr <Hhhr1214@gmail.com>
2021-05-13 11:09:29 +0800

    登录数据的修改

--- src/api/employees.js
+++ src/api/employees.js
@@ -40,4 +40,14 @@ export function addEmployee(data) {
     data
   })
 }
-
+/** *
+ *
+ * 保存员工的基本信息
+ * **/
+export function saveUserDetailById(data) {
+  return request({
+    url: `/sys/user/${data.id}`,
+    method: 'put',
+    data
+  })
+}
--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -5,15 +5,17 @@
         <el-tabs>
           <el-tab-pane label="登录账户设置">
             <!-- 放置表单 -->
-            <el-form :model="formData" :rules="rules" label-width="120px" style="margin-left: 120px; margin-top:30px">
+            <el-form ref="loginForm" :model="formData" :rules="rules" label-width="120px"
+                     style="margin-left: 120px; margin-top:30px"
+            >
               <el-form-item label="姓名:" prop="username">
                 <el-input v-model="formData.username" style="width:300px"/>
               </el-form-item>
-              <el-form-item label="密码:" prop="password">
-                <el-input v-model="formData.password" style="width:300px" type="password"/>
+              <el-form-item label="密码:" prop="password2">
+                <el-input v-model="formData.password2" style="width:300px" type="password"/>
               </el-form-item>
               <el-form-item>
-                <el-button type="primary">更新</el-button>
+                <el-button type="primary" @click="sendRequest">更新</el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
@@ -26,21 +28,44 @@
 </template>
 
 <script>
+import { getUserDetailById } from '@/api/user'
+import { saveUserDetailById } from '@/api/employees'
+
 export default {
   data() {
     return {
+      userId: this.$route.params.id,
       formData: {
         username: '',
-        password: ''
+        password2: ''
       },
       rules: {
         username: [
           { required: true, message: '该项不能为空', trigger: 'blur' }
         ],
-        password: [
+        password2: [
           { required: true, message: '该项不能为空', trigger: 'blur' }
         ]
       }
+    }
+  },
+  created() {
+    this.getUserInfo()
+  },
+  methods: {
+    async getUserInfo() {
+      this.formData = await getUserDetailById(this.userId)
+    },
+    async sendRequest() {
+      // 表单校验
+      await this.$refs.loginForm.validate()
+      // 发送请求
+      await saveUserDetailById({
+        ...this.formData,
+        password: this.formData.password2
+      })
+      // 提示用户
+      this.$message.success('修改成功')
     }
   }
 }
