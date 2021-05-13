dc438ca50dbb3f5b113367be666377c281be4683
Hhhr <Hhhr1214@gmail.com>
2021-05-13 10:09:43 +0800

    修改用户信息表单的基本布局和数据绑定

--- src/views/employees/detail.vue
+++ src/views/employees/detail.vue
@@ -4,22 +4,21 @@
       <el-card>
         <el-tabs>
           <el-tab-pane label="登录账户设置">
-
             <!-- 放置表单 -->
-            <el-form label-width="120px" style="margin-left: 120px; margin-top:30px">
-              <el-form-item label="姓名:">
-                <el-input style="width:300px" />
+            <el-form :model="formData" :rules="rules" label-width="120px" style="margin-left: 120px; margin-top:30px">
+              <el-form-item label="姓名:" prop="username">
+                <el-input v-model="formData.username" style="width:300px"/>
               </el-form-item>
-              <el-form-item label="密码:">
-                <el-input style="width:300px" type="password" />
+              <el-form-item label="密码:" prop="password">
+                <el-input v-model="formData.password" style="width:300px" type="password"/>
               </el-form-item>
               <el-form-item>
                 <el-button type="primary">更新</el-button>
               </el-form-item>
             </el-form>
           </el-tab-pane>
-          <el-tab-pane label="个人详情" />
-          <el-tab-pane label="岗位信息" />
+          <el-tab-pane label="个人详情"/>
+          <el-tab-pane label="岗位信息"/>
         </el-tabs>
       </el-card>
     </div>
@@ -28,7 +27,22 @@
 
 <script>
 export default {
-  name: 'detail',
+  data() {
+    return {
+      formData: {
+        username: '',
+        password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  }
 }
 </script>
 
