0812fd260cdc7c1cceaa85d744a189c8d1be0ce6
Hhhr <Hhhr1214@gmail.com>
2021-05-16 12:19:33 +0800

    权限点删除成功

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -14,10 +14,10 @@
           <el-table-column align="center" label="标识" prop="code"/>
           <el-table-column align="center" label="描述" prop="description"/>
           <el-table-column align="center" label="操作">
-            <template>
+            <template slot-scope="{row}">
               <el-button type="text">添加</el-button>
               <el-button type="text">编辑</el-button>
-              <el-button type="text">删除</el-button>
+              <el-button type="text" @click="delPermission(row.id)">删除</el-button>
             </template>
           </el-table-column>
 
@@ -28,7 +28,7 @@
 </template>
 
 <script>
-import { getPermissionList } from '@/api/permission'
+import { delPermission, getPermissionList } from '@/api/permission'
 import { listToTreeData } from '@/utils'
 
 export default {
@@ -44,6 +44,16 @@ export default {
     async getPermissionList() {
       this.list = listToTreeData(await getPermissionList(), '0')
       console.log(this.list)
+    },
+    async delPermission(id) {
+      // 二次校验
+      await this.$confirm('是否确认删除')
+      // 发请求
+      await delPermission(id)
+      // 提示用户
+      this.$message.success('删除成功')
+      // 重新加载数据
+      this.getPermissionList()
     }
   }
 }
