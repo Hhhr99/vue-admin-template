418689dd6146fa17b10e546d6335ede15211d3be
Hhhr <Hhhr1214@gmail.com>
2021-05-16 17:13:50 +0800

    弹窗添加完毕

--- src/views/permission/index.vue
+++ src/views/permission/index.vue
@@ -20,9 +20,36 @@
               <el-button type="text" @click="delPermission(row.id)">删除</el-button>
             </template>
           </el-table-column>
-
         </el-table>
       </el-card>
+      <!-- 添加/编辑弹窗 -->
+      <el-dialog :title="`${showText}权限点`" :visible="showDialog" @close="btnCancel">
+        <!-- 表单 -->
+        <el-form ref="perForm" :model="formData" :rules="rules" label-width="120px">
+          <el-form-item label="权限名称" prop="name">
+            <el-input v-model="formData.name" style="width:90%"/>
+          </el-form-item>
+          <el-form-item label="权限标识" prop="code">
+            <el-input v-model="formData.code" style="width:90%"/>
+          </el-form-item>
+          <el-form-item label="权限描述">
+            <el-input v-model="formData.description" style="width:90%"/>
+          </el-form-item>
+          <el-form-item label="开启">
+            <el-switch
+              v-model="formData.enVisible"
+              active-value="1"
+              inactive-value="0"
+            />
+          </el-form-item>
+        </el-form>
+        <el-row slot="footer" type="flex" justify="center">
+          <el-col :span="6">
+            <el-button size="small" type="primary" @click="btnOK">确定</el-button>
+            <el-button size="small" @click="btnCancel">取消</el-button>
+          </el-col>
+        </el-row>
+      </el-dialog>
     </div>
   </div>
 </template>
@@ -34,13 +61,46 @@ import { listToTreeData } from '@/utils'
 export default {
   data() {
     return {
-      list: []
+      list: [],
+      showDialog: false,
+      formData: {
+        name: '',
+        code: '',
+        description: '',
+        enVisible: '',
+
+        pid: '',
+        type: ''
+      },
+      rules: {
+        name: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ],
+        description: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ],
+        enVisible: [
+          { required: true, message: '该项不能为空', trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  computed: {
+    showText() {
+      return this.formData.id ? '编辑' : '新增'
     }
   },
   created() {
     this.getPermissionList()
   },
   methods: {
+    btnOK() {
+    },
+    btnCancel() {
+    },
     async getPermissionList() {
       this.list = listToTreeData(await getPermissionList(), '0')
       console.log(this.list)
