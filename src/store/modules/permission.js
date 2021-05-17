872b35f7cb11a171ac615709879f84bb7d8c8760
Hhhr <Hhhr1214@gmail.com>
2021-05-17 11:56:05 +0800

    路由筛选的逻辑

--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,6 +1,20 @@
+import { asyncRoutes } from '@/router'
+
 const state = {}
 const mutations = {}
-const actions = {}
+const actions = {
+  // 封装一个函数可以根据用户数据
+  // 筛选出他有权限访问的页面路由配置
+  // 1. 用户资料 (调用的时候传进来)
+  // 2. 路由列表 (可以通过直接 import )
+  filterRoutes(store, menus) {
+    // 应该配合全部的动态路由列表
+    // 和用户的 menus 权限数组
+    // 筛选出名字存在于 menus 里面的路由
+    const res = asyncRoutes.filter(route => menus.includes(route.name))
+    console.log(res)
+  }
+}
 
 export default {
   namespaced: true,
