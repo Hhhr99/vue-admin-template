ba7a3060f1c48316accba66b498460b59abe9bc3
Hhhr <Hhhr1214@gmail.com>
2021-05-17 11:56:44 +0800

    获取用户资料之后, 进入页面之前进行路由权限的筛选

--- src/permission.js
+++ src/permission.js
@@ -20,7 +20,16 @@ router.beforeEach(async(to, from, next) => {
       // 直接判断一个空对象, 其实永远是 true
       // 只能判断里面的其中一个属性在不在
       if (!store.state.user.userInfo.userId) {
-        await store.dispatch('user/getUserInfo')
+        // await store.dispatch('user/getUserInfo')
+        const res = await store.dispatch('user/getUserInfo')
+        // 如果 action return 了数据, 这边可以直接获取
+        console.log('触发筛选过程')
+        await store.dispatch('permission/filterRoutes', res.roles.menus)
+        // store.state.user.userInfo.roles.menus
+        // 这里是第一次进入页面(刷新)获取用户资料
+        // 用户资料里面有权限列表可以用来做筛选
+        // 将筛选的逻辑放入到 vuex 里面
+        // 需要准备两个数据 1. 全部的动态路由列表 2. 当前用户的信息
       }
       next()
     }
