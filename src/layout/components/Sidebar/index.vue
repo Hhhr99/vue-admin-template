aaf6dc06e44e2d6b14b896dbcfa19d962d06aae7
Hhhr <Hhhr1214@gmail.com>
2021-05-17 12:00:01 +0800

    菜单的渲染

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -28,11 +28,13 @@ export default {
   components: { SidebarItem, Logo },
   computed: {
     ...mapGetters([
-      'sidebar'
+      'sidebar',
+      'routes'
     ]),
-    routes() {
-      return this.$router.options.routes
-    },
+    // routes() {
+    //   // return this.$router.options.routes
+    //   return this.$store.getters.routes
+    // },
     activeMenu() {
       const route = this.$route
       const { meta, path } = route
--- src/store/modules/permission.js
+++ src/store/modules/permission.js
@@ -1,7 +1,17 @@
-import { asyncRoutes } from '@/router'
+import { constantRoutes, asyncRoutes } from '@/router'
 
-const state = {}
-const mutations = {}
+const state = {
+  // 这里创建一个变量
+  // 储存我们自己筛选完毕的菜单
+  // 供 sidebar 渲染
+  // 替代默认routes 选项
+  routes: []
+}
+const mutations = {
+  setRoutes(state, data) {
+    state.routes = [...constantRoutes, ...data]
+  }
+}
 const actions = {
   // 封装一个函数可以根据用户数据
   // 筛选出他有权限访问的页面路由配置
@@ -13,6 +23,7 @@ const actions = {
     // 筛选出名字存在于 menus 里面的路由
     const res = asyncRoutes.filter(route => menus.includes(route.name))
     console.log(res)
+    store.commit('setRoutes', res)
     return res
   }
 }
