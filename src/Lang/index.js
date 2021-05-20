a857a057dbb859ed7a27c37e7fc51ad13846d1d9
Hhhr <Hhhr1214@gmail.com>
2021-05-20 12:07:14 +0800

    合并菜单翻译包

--- src/Lang/index.js
+++ src/Lang/index.js
@@ -6,21 +6,28 @@ Vue.use(VueI18n)
 // 引入饿了么预设语言包
 import elementZH from 'element-ui/lib/locale/lang/zh-CN'
 import elementEN from 'element-ui/lib/locale/lang/en'
+
+// 引入自定义菜单的语言包
+import customZH from './zh'
+import customEN from './en'
+
 // 3. 创建实例
 export default new VueI18n({
   // 1. 设定当前语言
-  locale: 'en',
+  locale: 'cn',
   // 2. 设定字典
   messages: {
     zh: {
       hi: '你好丫',
       gd: '祝你开心每一天',
-      ...elementZH
+      ...elementZH,
+      ...customZH
     },
     en: {
       hi: 'WHat\'s up',
       gd: 'Have a good day',
-      ...elementEN
+      ...elementEN,
+      ...customEN
     }
   }
 })
