301aa469b7b69a4ccc551ffda3b414ba921cc659
Hhhr <Hhhr1214@gmail.com>
2021-05-20 10:03:59 +0800

    翻译原理的说明

--- src/Lang/index.js
+++ src/Lang/index.js
@@ -5,3 +5,20 @@ import VueI18n from 'vue-i18n'
 Vue.use(VueI18n)
 // 3. 创建实例
 export default new VueI18n()
+
+// 翻译原理的说明
+// // 1. 渲染的地方不能写死, 而是调用翻译函数
+// <div>{{translate('hi')}}</div>
+
+// // 2. 准备字典供翻译工具查询
+// const dict = {
+//   zh: {
+//     hi: '你好'
+//   },
+//   en: {
+//     hi: 'Hello'
+//   }
+// }
+
+// // 3. 可以随时改变当前语言的设定
+// const locale = 'en'
